be475c409f1a8259ced4ccc3f1fcda81e77b6167
monalinanda <monalinanda95@gmail.com>
2024-06-14 15:34:58 +0530

    add verciel api

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import appStore from "./utils/appStore.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
 	// TODO => Update the uri on production
-	uri: "http://localhost:4000/graphql" ,  // the URL of our GraphQL server.
+	uri: "https://leave-management-system-cb1j.vercel.app/graphql" ,  // the URL of our GraphQL server.
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
 	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
