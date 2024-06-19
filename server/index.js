83e05a89d2d0a73c9f4502bf2ac75239ecfc8fc3
Monali Nanda <35160281+monalinanda@users.noreply.github.com>
2024-06-19 22:47:52 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use(
             httpOnly: false,
             sameSite: 'none',
             secure: true,
-            domain:"https://leave-management-system-backend.vercel.app/"
+            domain:"leave-management-system-backend.vercel.app"
         },
         store: store,
     })
