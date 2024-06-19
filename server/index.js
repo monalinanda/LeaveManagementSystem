631f083abdd86054246e2d9db7a49162761fcdb3
Monali Nanda <35160281+monalinanda@users.noreply.github.com>
2024-06-19 22:58:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(
             maxAge: 1000 * 60 * 60 * 24 * 7,
             httpOnly: false,
             sameSite: 'none',
-            secure: true,
+            secure: false,
         },
         store: store,
     })
