fda411aaaec68bd861d1a37f41604a746c731982
Monali Nanda <35160281+monalinanda@users.noreply.github.com>
2024-06-19 12:00:23 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,8 @@ app.use(
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7,
       httpOnly: true, // this option prevents the Cross-Site Scripting (XSS) attacks
-      sameSite: 'none'
+      sameSite: 'none',
+      secure: true
     },
     store: store,
   })
