26e993f8ea55a8502ae3dc224fd791643f2653f5
Monali Nanda <35160281+monalinanda@users.noreply.github.com>
2024-06-19 22:52:51 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
 import appStore from "./utils/appStore.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 const client = new ApolloClient({
-	// TODO => Update the uri on production
-	uri: " https://leave-management-system-backend.vercel.app/graphql",   // the URL of our GraphQL server.
-	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
-	credentials: "include" , // This tells Apollo Client to send cookies along with every request to the server.
-	 headers: {
-        'Content-Type': 'application/json',
-    },
-	//  fetchOptions: {
- //       		 mode: 'no-cors'
- //      }
+  uri: " https://leave-management-system-backend.vercel.app/graphql", // the URL of our GraphQL server.
+  cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+  credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <ApolloProvider client={client}>
-     <Provider store={appStore}>
-    <App />
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ApolloProvider client={client}>
+    <Provider store={appStore}>
+      <App />
     </Provider>
-    </ApolloProvider>
-)
+  </ApolloProvider>,
+);
