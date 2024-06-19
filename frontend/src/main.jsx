63508eb01ea4f73914248568fa94c08348735a8a
Monali Nanda <35160281+monalinanda@users.noreply.github.com>
2024-06-19 18:18:48 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,10 +9,10 @@ const client = new ApolloClient({
 	// TODO => Update the uri on production
 	uri: " https://leave-management-system-backend.vercel.app/graphql",   // the URL of our GraphQL server.
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
-	credentials: "include" , // This tells Apollo Client to send cookies along with every request to the server.
-	 fetchOptions: {
-       		 mode: 'no-cors'
-      }
+	// credentials: "include" , // This tells Apollo Client to send cookies along with every request to the server.
+	//  fetchOptions: {
+ //       		 mode: 'no-cors'
+ //      }
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
