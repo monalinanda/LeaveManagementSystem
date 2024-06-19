27560ba3bb05519a247758e0605099829a5ee2f6
Monali Nanda <35160281+monalinanda@users.noreply.github.com>
2024-06-19 18:30:16 +0530

    Update connectDB.js

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -2,13 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
 	try {
-
- await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-		
-		console.log("MongoDB Connected");
+	const conn = await mongoose.connect(process.env.MONGO_URI);
+	console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (err) {
 		console.error(`Error: ${err.message}`);
 		process.exit(1);
