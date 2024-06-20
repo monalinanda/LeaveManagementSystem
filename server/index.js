d06981b39a318c8a2f04e9cc17cfc1cdc268564e
Monali Nanda <35160281+monalinanda@users.noreply.github.com>
2024-06-20 12:00:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ app.use(
             maxAge: 1000 * 60 * 60 * 24 * 7,
            // httpOnly: false,
             sameSite: 'none',
-            secure: false,
+            secure: true,
            // domain: 'leave-management-system-frontend.vercel.app'
             
         },
